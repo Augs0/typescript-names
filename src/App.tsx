897c32746b113fc24d16658ee9418ba09fa5a3a7
August <hachigatsu14@outlook.com>
2021-09-13 16:51:08 +0100

    final version of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import './App.css';
-import {useState} from 'react';
-import boysData from './data/boys-name.json';
-import girlsData from './data/girls-names.json'
+import React from 'react';
 
 import Search from './components/Search';
 
 function App() {
-  const [usersName, setUsersName] = useState('');
+  const [usersName, setUsersName] = React.useState<string>('');
+  const [message, setMessage] = React.useState<string>('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Popular names - 90s edition</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Popular names in the UK - 90s edition</h1>
         <p>Find out if your name is one of the top 10 from 1999!</p>
       </header>
-      <main id="main">
-        <Search setUsersName={setUsersName}/>
-        {usersName}
+      <main id='main'>
+        <Search
+          setMessage={setMessage}
+          usersName={usersName}
+          setUsersName={setUsersName}
+        />
+        <p className='msg'> {message}</p>
       </main>
-      <footer className="footer">
-        Data from <a href="/www.ons.gov.uk">ONS</a> 1999 datasets
+      <footer className='footer'>
+        Data from <a href='/www.ons.gov.uk'>ONS</a> 1999 datasets
       </footer>
     </div>
   );
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,20 +1,59 @@
-interface Props  {
-    setUsersName(arg: string): void;
+import { boysNames } from '../data/boys-name.json';
+import { girlsNames } from '../data/girls-names.json';
+
+interface Props {
+  setUsersName(arg: string): void;
+  usersName: string;
+  setMessage(arg: string): void;
 }
 
-const Search:React.FC<Props> = ({setUsersName}) => {
-    
-    const handleInput = (e: React.ChangeEvent<HTMLInputElement> ) => {
-       let grabName = e.target.value;
-       setUsersName(grabName)
+const Search: React.FC<Props> = ({ usersName, setUsersName, setMessage }) => {
+  const checkNameRanking = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    for (let i = 0; i < boysNames.length; i++) {
+      if (boysNames[i].name === usersName) {
+        let pos = boysNames[i].position;
+        setMessage(
+          `${usersName} was in the top ten boys names in 1999, at position ${pos}`
+        );
+        return;
+      } else {
+        setMessage(`${usersName} was not in the top ten in 1999!`);
+      }
     }
 
-    return (
-        <form>
-            <label htmlFor="usersName">Enter your name</label>
-            <input onChange={handleInput} type="text" name="usersName" id="usersName" />
-        </form>
-    )
-}
+    for (let i = 0; i < girlsNames.length; i++) {
+      if (girlsNames[i].name === usersName) {
+        let pos = girlsNames[i].position;
+        setMessage(
+          `${usersName} was in the top ten girls names in 1999, at position ${pos}`
+        );
+        return;
+      } else {
+        setMessage(`${usersName} was not in the top ten in 1999!`);
+      }
+    }
+  };
 
-export default Search
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let grabName = e.target.value;
+    setUsersName(grabName);
+  };
+
+  return (
+    <form onSubmit={checkNameRanking}>
+      <label htmlFor='usersName'>Enter your name</label>
+      <input
+        onChange={handleInput}
+        type='text'
+        name='usersName'
+        id='usersName'
+      />
+      <button className='btn' type='submit'>
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default Search;
